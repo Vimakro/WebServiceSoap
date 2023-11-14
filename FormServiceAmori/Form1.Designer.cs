1edfc4fabb448d84bb1fcbf764589da9c865b9c2
Vimakro <131078738+Vimakro@users.noreply.github.com>
2023-11-14 10:00:17 -0300

    Add files via upload

---
+++ FormServiceAmori/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace FormServiceAmori
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_Fecha = new System.Windows.Forms.TextBox();
+            this.txt_Referencia = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fecha";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Referencia";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(49, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Martin Amori";
+            // 
+            // txt_Fecha
+            // 
+            this.txt_Fecha.Enabled = false;
+            this.txt_Fecha.Location = new System.Drawing.Point(16, 57);
+            this.txt_Fecha.Name = "txt_Fecha";
+            this.txt_Fecha.Size = new System.Drawing.Size(167, 20);
+            this.txt_Fecha.TabIndex = 3;
+            // 
+            // txt_Referencia
+            // 
+            this.txt_Referencia.Enabled = false;
+            this.txt_Referencia.Location = new System.Drawing.Point(16, 112);
+            this.txt_Referencia.Name = "txt_Referencia";
+            this.txt_Referencia.Size = new System.Drawing.Size(167, 20);
+            this.txt_Referencia.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(195, 202);
+            this.Controls.Add(this.txt_Referencia);
+            this.Controls.Add(this.txt_Fecha);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Servicio Amori";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_Fecha;
+        private System.Windows.Forms.TextBox txt_Referencia;
+    }
+}
+
